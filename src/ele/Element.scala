750128fa66a5fddb090cdf0c1e63f5f141cdc794
zhangyibo <goldeneyezhang@163.com>
2020-01-05 20:24:02 +0800

    10.13 定义工厂对象

--- src/ele/Element.scala
+++ src/ele/Element.scala
@@ -6,4 +6,28 @@ abstract class Element {
   def width: Int = if (height == 0) 0 else contents(0).length
 
   def height: Int = contents.length
+
+  def above(that: Element): Element =
+    Element.elem(this.contents ++ that.contents)
+
+  def beside(that: Element): Element = {
+    Element.elem(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
+    )
+  }
+
+  override def toString = contents mkString "\n"
+}
+
+object Element {
+  def elem(contents: Array[String]): Element =
+    new ArrayElement(contents)
+
+  def elem(chr: Char, width: Int, height: Int): Element =
+    new UniformElement(chr, width, height)
+
+  def elem(line: String): Element =
+    new LineElement(line)
 }
