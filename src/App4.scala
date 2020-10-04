156cdcbe8a2f93f766c38c7907c04f3e184905ec
zhangyyibo <goldeneyezhang@163.com>
2020-10-04 08:40:52 +0800

    使用列表merge

--- src/App4.scala
+++ src/App4.scala
@@ -9,8 +9,15 @@ object App4 {
     System.out.println(diag3.flatten)
     System.out.println(isort2(List(5, 6, 3, 8, 20, 2)))
     System.out.println(fruit zip diag3)
+    System.out.println(msort((x: Int, y: Int) => x < y)(List(5, 7, 1, 3)))
   }
 
+  /**
+   * 插入排序
+   *
+   * @param xs
+   * @return
+   */
   def isort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) Nil
     else insert(xs.head, xs.tail)
@@ -30,4 +37,29 @@ object App4 {
     else y :: insert2(x, ys)
   }
 
+  /**
+   *
+   * @param less
+   * @param xs
+   * @tparam T
+   * @return
+   */
+  def msort[T](less: (T, T) => Boolean)
+              (xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] =
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x :: xs1, y :: ys1) =>
+          if (less(x, y)) x :: merge(xs1, ys)
+          else y :: merge(xs, ys1)
+      }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(msort(less)(ys), msort(less)(zs))
+    }
+  }
 }
