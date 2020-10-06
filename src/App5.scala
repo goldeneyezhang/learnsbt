b9c22d0a18b36611cbc69b1ac91d1872654d8e40
zhangyyibo <goldeneyezhang@163.com>
2020-10-06 16:59:40 +0800

    Wire

--- src/App5.scala
+++ src/App5.scala
@@ -1,5 +1,11 @@
 object App5 {
   def main(args: Array[String]): Unit = {
-
+    val account = new BankAccount()
+    account deposit 100
+    println(account withdraw 80)
+    println(account withdraw 80)
+    val t = new Thermometer
+    t.celsius = 100
+    println(t)
   }
 }
---
+++ src/BasicCircuitSimulation.scala
@@ -0,0 +1,37 @@
+abstract class BasicCircuitSimulation extends Simulation {
+  def InverterDelay: Int
+
+  def AndGateDelay: Int
+
+  def OrGateDelay: Int
+
+  class Wire {
+    private var sigVal = false
+    private var actions: List[Action] = List()
+
+    def getSignal = sigVal
+
+    def setSignal(s: Boolean) =
+      if (s != sigVal) {
+        sigVal = s
+        actions foreach (_ ())
+      }
+
+    def addAction(a: Action) = {
+      actions = a :: actions
+      a()
+    }
+  }
+
+  def inverter(input: Wire, output: Wire) = {
+    def invertAction() = {
+      val inputSig = input.getSignal
+      afterDelay(InverterDelay) {
+        output setSignal !inputSig
+      }
+    }
+
+    input addAction invertAction
+  }
+
+}
