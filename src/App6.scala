1a1183843f22a6e2228064fea796f36a594ba2fe
zhangyibo <goldeneyezhang@163.com>
2020-10-18 10:11:29 +0800

    向量

--- src/App6.scala
+++ src/App6.scala
@@ -1,3 +1,5 @@
+
+
 object App6 extends App {
   for(x <- List(1,2);y <- List("one","two"))
     yield(x,y)
@@ -8,4 +10,13 @@ object App6 extends App {
   val sit = xs sliding 3
   println(sit.next())
   println(sit.next())
+  //流
+  val str = 1#::2#::3#::Stream.Empty
+  println(str)
+  println(str.toList)
+  //向量
+  val vec = scala.collection.immutable.Vector.empty
+  val vec2 = vec :+ 1 :+2
+  val vec3 = 100 +: vec2
+  println(vec3)
 }
